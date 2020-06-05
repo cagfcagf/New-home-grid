d303f29008bd401edcd511198bcd5d7a981fe3ea
cagfcagf <cagfeijoo@gmail.com>
2020-06-05 18:18:55 -0400

    faq

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 
+function mostrarOcultar(Faq){
+var elemento = document.getElementById(Faq);
+if(!elemento) {
+return true;
+}
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
