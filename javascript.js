3b4298133fee93a94b23fc2f27c7b1b1b2608cb5
cagfcagf <cagfeijoo@gmail.com>
2020-06-06 19:22:14 -0400

    toggle

--- javascript.js
+++ javascript.js
@@ -2,14 +2,17 @@
 // Muestra y oculta las preguntas frecuentes
 
 function mostrarOcultar(Faq){
-var elemento = document.getElementById(Faq);
+const elemento = document.getElementById(Faq);
+const icon = document.getElementById('icon');
 if(!elemento) {
 return true;
 }
 if (elemento.style.display == "none") {
 elemento.style.display = "block"
+icon.classList.add('less');
 } else {
 elemento.style.display = "none"
+icon.classList.remove('less');
 };
 return true;
 };
