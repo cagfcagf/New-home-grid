85aafa19aec28e014cd83c693f0fb44af089f943
cagfcagf <cagfeijoo@gmail.com>
2020-06-08 15:31:26 -0400

    footer 3

--- javascript.js
+++ javascript.js
@@ -16,3 +16,19 @@ icono.classList.remove('less');
 };
 return true;
 };
+
+function rotarFlecha(address, arrow){
+const direccion = document.getElementById(address);
+const flecha = document.getElementById(arrow);
+if(!direccion) {
+return true;
+}
+if (direccion.style.display == "none") {
+direccion.style.display = "block"
+flecha.classList.add('rotar');
+} else {
+direccion.style.display = "none"
+flecha.classList.remove('rotar');
+};
+return true;
+};
